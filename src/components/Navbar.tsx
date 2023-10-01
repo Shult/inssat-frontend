abbc43bbe664c937323f5201c900714c0657eddd
Shult <90676302+Shult@users.noreply.github.com>
2023-10-01 13:24:02 +0200

    Init commit : Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,6 @@ const Navbar = () => {
     return(
         <nav className="">
             <div className="">
-                <a className="">INSSAT</a>
                 <ul className="">
                     <li><a href="/homeStudent">HomeStudent</a></li>
                     <li><a href="/test">Test</a></li>
