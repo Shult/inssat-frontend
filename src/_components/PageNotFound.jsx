641253993e91e2dc28518c117d73f22fc1aa373b
wudline <wudline.bf@gmail.com>
2023-10-25 09:05:18 +0200

    feat/ PageNotFound created and Router redirect every undefined routes

--- src/_components/PageNotFound.jsx
+++ src/_components/PageNotFound.jsx
@@ -1,15 +1,10 @@
-import {getElementError} from "@testing-library/react";
-
 const PageNotFound = () => {
-    const error = getElementError()
-
     return (
         <div>
             <h1>Oops...</h1>
-            <p>Something went wrong: {error.message}</p>
+            <p>Sorry, an error has occured.</p>
         </div>
     );
-
 }
 
 export default PageNotFound
