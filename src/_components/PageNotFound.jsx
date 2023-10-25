8905a083c5f1c945ae05606e51d3962c06be123a
wudline <wudline.bf@gmail.com>
2023-10-25 09:05:18 +0200

    feat/ PageNotFound created and Router redirect every undefined routes

---
+++ src/_components/PageNotFound.jsx
@@ -0,0 +1,12 @@
+const PageNotFound = () => {
+
+    return (
+        <div>
+            <h1>Oops...</h1>
+            <p>Sorry, an error has occured.</p>
+        </div>
+    );
+
+}
+
+export default PageNotFound
--- src/_navigation/Router.tsx
+++ src/_navigation/Router.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import {Route, Routes} from "react-router-dom";
 import HomeStudent from "../HomeStudent";
 import Test from "../test/Test";
+import PageNotFound from "../_components/PageNotFound";
 
 const Router = () => {
     return(
         <Routes>
             <Route path="/homeStudent" Component={HomeStudent}/>
             <Route path="/test" Component={Test}/>
+            <Route path="*" Component={PageNotFound} />
         </Routes>
     )
 }
