61b995c7d99446a9916c8a6d9d64a61758fcfef3
wudline <wudline.bf@gmail.com>
2023-10-26 11:19:14 +0200

    fix/ cleaned unsed files

--- src/_store/store.tsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import calendarReducer from "../_components/Calendar/Calendar";
-
-export default configureStore({
-    reducer: {
-        calendar: calendarReducer,
-    },
-})
