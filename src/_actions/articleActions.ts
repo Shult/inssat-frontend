59670a0330dfff24d60ab140a25972db7eed7b33
MESTRE Sylvain <mestres.sin@gmail.com>
2023-10-27 17:01:55 +0200

    Merge : oups

--- src/_actions/articleActions.ts
+++ src/_actions/articleActions.ts
@@ -3,29 +3,29 @@ import {FETCH_ARTICLES_REQUEST, FETCH_ARTICLES_SUCCESS, FETCH_ARTICLES_FAILURE, 
 import articles from '../_data/articles.json';
 import { Article, CreateArticleAction, UpdateArticleAction, DeleteArticleAction } from '../_interfaces/interfacesArticles';
 
-// export const getArticles = () => {
-//     return {
-//         type: FETCH_ARTICLES_SUCCESS,
-//         payload: articles as Article[]
-//     };
-// };
-// Avec la gestion d'erreur
 export const getArticles = () => {
-    return async (dispatch : any)=> {
-        dispatch({ type: FETCH_ARTICLES_REQUEST, payload:"" });
-        try{
-            dispatch({
-                type: FETCH_ARTICLES_SUCCESS,
-                payload: articles as Article[]
-            })
-        }catch (e){
-            dispatch({
-                type: FETCH_ARTICLES_FAILURE,
-                payload: "error"
-            })
-        }
+    return {
+        type: FETCH_ARTICLES_SUCCESS,
+        payload: articles as Article[]
     };
 };
+// Avec la gestion d'erreur
+// export const getArticles = () => {
+//     return async (dispatch : any)=> {
+//         dispatch({ type: FETCH_ARTICLES_REQUEST, payload:"" });
+//         try{
+//             dispatch({
+//                 type: FETCH_ARTICLES_SUCCESS,
+//                 payload: articles as Article[]
+//             })
+//         }catch (e){
+//             dispatch({
+//                 type: FETCH_ARTICLES_FAILURE,
+//                 payload: "error"
+//             })
+//         }
+//     };
+// };
 
 // When the API will be available
 // export const getArticlesAPI = () => {
