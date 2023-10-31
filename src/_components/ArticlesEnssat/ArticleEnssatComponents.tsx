42dee43c177fe953632506dcbeb11907f43daf96
MESTRE Sylvain <mestres.sin@gmail.com>
2023-10-31 20:39:51 +0100

    feat: pagination

--- src/_components/ArticlesEnssat/ArticleEnssatComponents.tsx
+++ src/_components/ArticlesEnssat/ArticleEnssatComponents.tsx
@@ -28,16 +28,31 @@ const ArticlesComponent = () => {
         }
     }, []);
 
+    const [currentPage, setCurrentPage] = React.useState(1);
+    const ARTICLES_PER_PAGE = 3;
+    const startIndex = (currentPage - 1) * ARTICLES_PER_PAGE;
+    const endIndex = startIndex + ARTICLES_PER_PAGE;
+    const currentArticles = articles.slice(startIndex, endIndex);
+
 
     console.log(articles);
     if(status==="succeeded"){
         return (
             <div className={"container"}>
-                {articles.map((item, index)=>(
+                <div>
+                    {Array(Math.ceil(articles.length / ARTICLES_PER_PAGE)).fill(null).map((_, idx) => (
+                        <button onClick={() => setCurrentPage(idx + 1)}>
+                            {idx + 1}
+                        </button>
+                    ))}
+                </div>
+                {currentArticles.map((item, index) => (
                     <div className={"col"}>
                         <ArticleCardEnssat item={item} index={index}/>
                     </div>
                 ))}
+
+
             </div>
         );
     } else {
