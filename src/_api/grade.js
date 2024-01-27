dc97c20198867af780a64b8eb7ca37fe9aa64683
antoineMaurais <antoine.maurais98@gmail.com>
2024-01-27 19:49:43 +0100

    Connexion avec le back pour la partie pre-ticket partie 1

--- src/_api/grade.js
+++ src/_api/grade.js
@@ -2,8 +2,10 @@ import {apiAcademy} from './client'
 
 const getGradeById = (id) => apiAcademy.get(`/grades/${id}`)
 const getAllGrades = () => apiAcademy.get(`/grades`)
+const getGradesByUserId = (id) => apiAcademy.get(`/grades/tickets/${id}`)
 
 export {
     getGradeById,
-    getAllGrades
+    getAllGrades,
+    getGradesByUserId
 }
